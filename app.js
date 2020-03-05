6d72278ed4b85693fe6af23bc1980c0b5fee4c4d
1gilmar <1gilmar@gmail.com>
2020-03-05 19:10:07 -0300

    ajustado o processador

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ var leitor = new Ler();
 
 async function main(){
     var dados = await leitor.Reader("./arquivo.cvs");
-    console.log(dados);
-
-    processo.Processar(dados)
+    var dadosprocessados = processo.Processar(dados)
+    console.log(dadosprocessados)
 
 }
 
